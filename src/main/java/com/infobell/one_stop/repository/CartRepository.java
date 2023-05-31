d73f090c6e163f8b5d2fc44487f9058bbb99393c
Shubhambal <113638209+Shubhambal@users.noreply.github.com>
2023-05-31 12:18:05 +0530

    Delete src directory

--- src/main/java/com/infobell/one_stop/repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.infobell.one_stop.repository;
-
-import com.infobell.one_stop.model.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CartRepository extends JpaRepository<Cart, Integer> {
-}
