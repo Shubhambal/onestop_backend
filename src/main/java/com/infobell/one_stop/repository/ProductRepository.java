d06cbd91d72041a8857f7fafb644aa7b2c599148
Shubhambal <113638209+Shubhambal@users.noreply.github.com>
2023-05-31 12:15:45 +0530

    Delete src directory

--- src/main/java/com/infobell/one_stop/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.infobell.one_stop.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.infobell.one_stop.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Integer>{
-    Product findByProductId(String i);
-}
