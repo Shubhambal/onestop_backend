a86201b152e90bbc0c21f18331c6332f60c019cc
Shubhambal <113638209+Shubhambal@users.noreply.github.com>
2023-05-31 12:12:42 +0530

    Delete src directory

--- src/main/java/com/infobell/one_stop/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.infobell.one_stop.repository;
-
-import com.infobell.one_stop.model.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentRepository extends JpaRepository<Payment, Integer> {
-}
