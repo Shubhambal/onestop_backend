c140255b8700ed6d79a8ea5b0e1f3055ced34648
Devesh <ds8898382@gmail.com>
2023-06-08 15:45:32 +0530

    Product Test Cases Updated

--- src/test/java/com/emart/ProductControllerTest.java
+++ src/test/java/com/emart/ProductControllerTest.java
@@ -2,10 +2,18 @@ package com.emart;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.List;
+
+import org.json.JSONException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 
 import com.emart.entities.Product;
 
@@ -13,87 +21,86 @@ import static io.restassured.RestAssured.given;
 
 public class ProductControllerTest {
 
-    @BeforeAll
-    public static void setup() {
-        RestAssured.baseURI = "http://localhost:8080"; // Update the base URL to match your application's URL
-    }
+//	@BeforeAll
+//	public static void setup() {
+//		RestAssured.baseURI = "http://localhost:8080"; // Update the base URL to match your application's URL
+//	}
 
-    @Test
-    public void testShowProducts() {
-        Response response = given()
-                .contentType(ContentType.JSON)
-                .when()
-                .get("/api/products")
-                .then()
-                .extract().response();
+	 /**
+     * This test case is use to get all product from database.
+     *
+     * It will return the data with status code 200.
+     */
+	
+	@Test
+	public void testShowProducts() {
+		Response response = given().contentType(ContentType.JSON).when().get("/api/products").then().extract()
+				.response();
 
-        Assertions.assertEquals(200, response.getStatusCode());
-        // Add additional assertions to validate the response body or structure
-    }
+		Assertions.assertEquals(200, response.getStatusCode());
+		// Add additional assertions to validate the response body or structure
+		
+		// Assuming you expect the response body to have a "products" array
+		JsonPath jsonPath = response.jsonPath();
 
-    @Test
-    public void testGetProduct() {
-        int productId = 4; // Update with an existing product ID
+		Assertions.assertTrue(jsonPath.getList("products") instanceof List, "Expected 'products' to be an array.");
+		Assertions.assertTrue(jsonPath.getList("products").size() > 0, "Expected 'products' array to be non-empty.");
 
-        Response response = given()
-                .contentType(ContentType.JSON)
-                .when()
-                .get("/api/productsById/" + productId)
-                .then()
-                .extract().response();
+	   response.prettyPrint();
+	}
+	
+	
+	/**
+     * This test case is use to get product of particular id from database.
+     *
+     * It will return the data with status code 200.
+     */
+	
+	@Test
+	public void testGetProduct() {
+		int productId = 2; // Update with an existing product ID
 
-        Assertions.assertEquals(200, response.getStatusCode());
-        // Add additional assertions to validate the response body or structure
-    }
+		Response response = given().contentType(ContentType.JSON).when().get("/api/productsById/" + productId).then()
+				.extract().response();
 
-    @Test
-    public void testRemoveProduct() {
-        int productId = 2; // Update with an existing product ID
+		Assertions.assertEquals(200, response.getStatusCode());
+		// Add additional assertions to validate the response body or structure
+		
+		 String expectedJson = "{ \"key\": \"value\" }";
 
-        Response response = given()
-                .contentType(ContentType.JSON)
-                .when()
-                .delete("/api/products/" + productId)
-                .then()
-                .extract().response();
+	        String responseBody = response.getBody().asString();
+	        System.out.println("Actual Response Body: " +responseBody);
+	}
+	
+	/**
+     * It is negative testing for get product by id .
+     *
+     * It will return status port 404.
+     */
+	
+	@Test
+	public void testGetProductById_Negative() {
+	    RestAssured.baseURI = "http://localhost:8080/api/productsById/";
 
-        Assertions.assertEquals(200, response.getStatusCode());
-        // Add additional assertions to validate the response body or structure
-    }
+	    int productId = 999; // Non-existing customer ID
 
-    @Test
-    public void testAddProduct() {
-        Product product = new Product(4,"TV",30000.0f,null,false,false);
-        // Set the necessary properties of the Product object
+	    Response response = given()
+	            .pathParam("p_Id", productId)
+	            .when()
+	            .get("/{p_Id}");
 
-        Response response = given()
-                .contentType(ContentType.JSON)
-                .body(product)
-                .when()
-                .post("/api/products")
-                .then()
-                .extract().response();
+	    response.prettyPrint();
+	    System.out.println("Status code = " + response.getStatusCode());
+	}
+	
+	
 
-        Assertions.assertEquals(200, response.getStatusCode());
-        // Add additional assertions to validate the response body or structure
-    }
-
-//    @Test
-//    public void testGetProductsByCategory() {
-//        int categoryId = 1; // Update with an existing category ID
-//
-//        Response response = given()
-//                .contentType(ContentType.JSON)
-//                .when()
-//                .get("/api/productsByCat/" + categoryId)
-//                .then()
-//                .extract().response();
-//
-//        Assertions.assertEquals(200, response.getStatusCode());
-//        // Add additional assertions to validate the response body or structure
-//    }
-
-    @Test
+	/**
+     * This test case is use to search product from given keyword from database.
+     *
+     * It will return the data with status code 200.
+     */
+	@Test
     public void testSearchProducts() {
         String keyword = "TV"; // Update with a search keyword
 
@@ -102,22 +109,89 @@ public class ProductControllerTest {
                 .when()
                 .get("/api/search/" + keyword)
                 .then()
-                .extract().response();
+                .extract()
+                .response();
 
         Assertions.assertEquals(200, response.getStatusCode());
-        // Add additional assertions to validate the response body or structure
+
+        // Assuming the expected JSON response is: { "key": "value" }
+        String expectedJson = "{ \"key\": \"value\" }";
+
+        String responseBody = response.getBody().asString();
+        System.out.println("Actual Response Body: " +responseBody);
+
     }
 
-//    @Test
-//    public void testGetProductsByPromotion() {
-//        Response response = given()
-//                .contentType(ContentType.JSON)
-//                .when()
-//                .get("/api/promotion")
-//                .then()
-//                .extract().response();
-//
-//        Assertions.assertEquals(200, response.getStatusCode());
-//        // Add additional assertions to validate the response body or structure
-//    }
+
+	
+	/**
+     * This test case is use to remove product from given id from database.
+     *
+     * It will return the data with status code 200.
+     */
+	
+	@Test
+	public void testRemoveProduct() {
+	    int productId = 4; // Update with an existing product ID
+
+	    Response response = given().contentType(ContentType.JSON)
+	            .when()
+	            .delete("/api/products/" + productId)
+	            .then()
+	            .log() 
+	            .all()
+	            .extract().response();
+
+	    Assertions.assertEquals(200, response.getStatusCode());
+	    Assertions.assertEquals("", response.getBody().asString());
+	    // Add additional assertions to validate the response body or structure
+	}
+
+	
+	/**
+     * This test case is use to do negative testing which is to remove the product which does
+     * not exist in database.
+     *
+     * It will return the data with status code 404.
+     */
+	
+	@Test
+	public void testRemoveProduct_Negative() {
+	    int productId = -1; // Non-existing product ID
+
+	    Response response = given()
+	            .contentType(ContentType.JSON)
+	            .when()
+	            .delete("/api/products/" + productId)
+	            .then()
+	            .extract()
+	            .response();
+
+	    Assertions.assertEquals(404, response.getStatusCode());
+	    // Add additional assertions or validations for the response body or structure
+	}
+
+
+	/**
+     * This test case is use to add product for given data to database.
+     *
+     * It will return the data with status code 200 for successfull addition of database .
+     */
+
+	@Test
+	public void testAddProduct() {
+		Product product = new Product(4, "TV", 30000.0f, null, false, false);
+		// Set the necessary properties of the Product object
+
+		Response response = given().contentType(ContentType.JSON).body(product).when().post("/api/products").then()
+				.extract().response();
+
+		Assertions.assertEquals(200, response.getStatusCode());
+		// Add additional assertions to validate the response body or structure
+		System.out.println("Product added successfully" );
+		
+	}
+
+
+
 }
